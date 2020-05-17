dbda9f41f8ad9f71602264b9c3b40d3d7a6a48f0
sanyi <sanyielte@gmail.com>
2020-05-17 21:39:55 +1000

    Updates

---
+++ precache-manifest.48fc918a41c259a2a44003cca29f2dc0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a132a6a92e14f2d39b2bbf4f98161408",
+    "url": "/ReactChallange/index.html"
+  },
+  {
+    "revision": "1aa3d9fc0a0a5abeeeec",
+    "url": "/ReactChallange/static/css/2.b0a67369.chunk.css"
+  },
+  {
+    "revision": "7c1f67f17e219fb71cba",
+    "url": "/ReactChallange/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1aa3d9fc0a0a5abeeeec",
+    "url": "/ReactChallange/static/js/2.dfcc1a82.chunk.js"
+  },
+  {
+    "revision": "a6d5bab58f82b51182f890af1f1a3df8",
+    "url": "/ReactChallange/static/js/2.dfcc1a82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c1f67f17e219fb71cba",
+    "url": "/ReactChallange/static/js/main.5f9b6b47.chunk.js"
+  },
+  {
+    "revision": "7ca1666b9e48cb44300b",
+    "url": "/ReactChallange/static/js/runtime-main.2ca745b5.js"
+  }
+]);
